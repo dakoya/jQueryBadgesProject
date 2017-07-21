50c2ff395c7d280db6e369d9b10ae0ff919d9c71
dakoya <david.mzt@hotmail.fr>
2017-07-21 17:21:40 +0200

    requête ajax

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,12 @@ $(function() {
 
   // your code will go here
 
+  $.ajax({
+    url: 'https://www.codeschool.com/users/da1224.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
+
 });
